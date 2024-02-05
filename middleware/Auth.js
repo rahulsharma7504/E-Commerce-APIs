93896ccd80010cdc23b09acfcc7d798300a41234
Rahul Sharma <rahul658541@gmail.com>
2024-02-05 19:57:41 +0530

    update

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,29 +1,23 @@
-const SecKey = require('../config/config');
 const jwt = require('jsonwebtoken');
+const SecKey = require('../config/config');
 
-const JWT = async (req, res, next) => {
-  
-  const Token = req.body.Authorization || req.query.Authorization || req.headers['Authorization'] || req.headers['Authorization']; // Also check Authorization header
-  if (!Token) {
-    return res.status(401).send({ msg: "Token not found" }); // Use 401 for unauthorized
-  }try {
-    const decoded = jwt.verify(Token, SecKey.secrateKey);
-    console.log(decoded.foo);
-    console.log('Decoded token:', decoded);
-    next();
-  } catch (error) {
-    if (error) {
-      return res.status(401).send('Token expired');
-    } else if (error instanceof jwt.JsonWebTokenError) {
-      return res.status(401).send('Invalid token');
-    } else {
-      console.error('Unexpected error:', error);
-      return res.status(500).send('Internal server error');
+const JWT = (req, res, next) => {
+    const token = req.headers['authorization'] || req.body.authorization || req.query.authorization;
+
+    if (!token) return res.status(401).send({ msg: 'Token not found' });
+
+    try {
+      let TOKEN = jwt.verify(token, SecKey.secrateKey);
+        console.log('Decoded token:', TOKEN);
+        next();
+    } catch (error) {
+        const responses = {
+            TokenExpiredError: 'Token expired',
+            JsonWebTokenError: 'Invalid token',
+        };
+
+        return res.status(401).send(responses[error.name] || 'Internal server error');
     }
-  }
-  
-    
-  
 };
 
 module.exports = JWT;
